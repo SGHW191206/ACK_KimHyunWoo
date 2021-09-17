304e2b6e0ba5ebeda7d07cf658bb07f851469c8d
김현우 <@HYUNWOO>
2021-09-17 19:26:24 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ACK_KimHyunWoo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ackDataSet = new ACK_KimHyunWoo.ackDataSet();
+            this.studentTableAdapter = new ACK_KimHyunWoo.ackDataSetTableAdapters.studentTableAdapter();
+            this.Btn_Read = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            this.Btn_Insert = new System.Windows.Forms.Button();
+            this.Btn_Delete = new System.Windows.Forms.Button();
+            this.ackDataSet1 = new ACK_KimHyunWoo.ackDataSet1();
+            this.studentBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.studentTableAdapter1 = new ACK_KimHyunWoo.ackDataSet1TableAdapters.studentTableAdapter();
+            this.gradeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.noDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.classDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scoreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ackDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ackDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gradeDataGridViewTextBoxColumn,
+            this.noDataGridViewTextBoxColumn,
+            this.classDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.scoreDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.studentBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 58);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(610, 230);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataMember = "student";
+            this.studentBindingSource.DataSource = this.ackDataSet;
+            // 
+            // ackDataSet
+            // 
+            this.ackDataSet.DataSetName = "ackDataSet";
+            this.ackDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentTableAdapter
+            // 
+            this.studentTableAdapter.ClearBeforeFill = true;
+            // 
+            // Btn_Read
+            // 
+            this.Btn_Read.Location = new System.Drawing.Point(668, 34);
+            this.Btn_Read.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Read.Name = "Btn_Read";
+            this.Btn_Read.Size = new System.Drawing.Size(85, 51);
+            this.Btn_Read.TabIndex = 1;
+            this.Btn_Read.Text = "Read";
+            this.Btn_Read.UseVisualStyleBackColor = true;
+            this.Btn_Read.Click += new System.EventHandler(this.Btn_Read_Click);
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.Location = new System.Drawing.Point(668, 114);
+            this.Btn_Update.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(85, 51);
+            this.Btn_Update.TabIndex = 2;
+            this.Btn_Update.Text = "Update";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            this.Btn_Update.Click += new System.EventHandler(this.Btn_Update_Click);
+            // 
+            // Btn_Insert
+            // 
+            this.Btn_Insert.Location = new System.Drawing.Point(668, 190);
+            this.Btn_Insert.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Insert.Name = "Btn_Insert";
+            this.Btn_Insert.Size = new System.Drawing.Size(85, 51);
+            this.Btn_Insert.TabIndex = 3;
+            this.Btn_Insert.Text = "Insert";
+            this.Btn_Insert.UseVisualStyleBackColor = true;
+            this.Btn_Insert.Click += new System.EventHandler(this.Btn_Insert_Click);
+            // 
+            // Btn_Delete
+            // 
+            this.Btn_Delete.Location = new System.Drawing.Point(668, 266);
+            this.Btn_Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Delete.Name = "Btn_Delete";
+            this.Btn_Delete.Size = new System.Drawing.Size(85, 51);
+            this.Btn_Delete.TabIndex = 4;
+            this.Btn_Delete.Text = "Delete";
+            this.Btn_Delete.UseVisualStyleBackColor = true;
+            this.Btn_Delete.Click += new System.EventHandler(this.Btn_Delete_Click);
+            // 
+            // ackDataSet1
+            // 
+            this.ackDataSet1.DataSetName = "ackDataSet1";
+            this.ackDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentBindingSource1
+            // 
+            this.studentBindingSource1.DataMember = "student";
+            this.studentBindingSource1.DataSource = this.ackDataSet1;
+            // 
+            // studentTableAdapter1
+            // 
+            this.studentTableAdapter1.ClearBeforeFill = true;
+            // 
+            // gradeDataGridViewTextBoxColumn
+            // 
+            this.gradeDataGridViewTextBoxColumn.DataPropertyName = "grade";
+            this.gradeDataGridViewTextBoxColumn.HeaderText = "grade";
+            this.gradeDataGridViewTextBoxColumn.Name = "gradeDataGridViewTextBoxColumn";
+            // 
+            // noDataGridViewTextBoxColumn
+            // 
+            this.noDataGridViewTextBoxColumn.DataPropertyName = "no";
+            this.noDataGridViewTextBoxColumn.HeaderText = "no";
+            this.noDataGridViewTextBoxColumn.Name = "noDataGridViewTextBoxColumn";
+            // 
+            // classDataGridViewTextBoxColumn
+            // 
+            this.classDataGridViewTextBoxColumn.DataPropertyName = "class";
+            this.classDataGridViewTextBoxColumn.HeaderText = "class";
+            this.classDataGridViewTextBoxColumn.Name = "classDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // scoreDataGridViewTextBoxColumn
+            // 
+            this.scoreDataGridViewTextBoxColumn.DataPropertyName = "score";
+            this.scoreDataGridViewTextBoxColumn.HeaderText = "score";
+            this.scoreDataGridViewTextBoxColumn.Name = "scoreDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 467);
+            this.Controls.Add(this.Btn_Delete);
+            this.Controls.Add(this.Btn_Insert);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.Btn_Read);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ackDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ackDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ackDataSet ackDataSet;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private ackDataSetTableAdapters.studentTableAdapter studentTableAdapter;
+        private System.Windows.Forms.Button Btn_Read;
+        private System.Windows.Forms.Button Btn_Update;
+        private System.Windows.Forms.Button Btn_Insert;
+        private System.Windows.Forms.Button Btn_Delete;
+        private ackDataSet1 ackDataSet1;
+        private System.Windows.Forms.BindingSource studentBindingSource1;
+        private ackDataSet1TableAdapters.studentTableAdapter studentTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gradeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn noDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn classDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scoreDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ACK_KimHyunWoo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 이 코드는 데이터를 'ackDataSet1.student' 테이블에 로드합니다. 필요 시 이 코드를 이동하거나 제거할 수 있습니다.
+            //this.studentTableAdapter1.Fill(this.ackDataSet1.student);
+
+        }
+
+        private void Btn_Read_Click(object sender, EventArgs e)
+        {
+            this.studentTableAdapter.Fill(this.ackDataSet.student);
+            MessageBox.Show("Read 성공");
+        }
+
+        private void Btn_Update_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.studentBindingSource.EndEdit();
+            this.studentTableAdapter.Update(this.ackDataSet);
+            MessageBox.Show("Update 성공");
+        }
+
+        private void Btn_Insert_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.studentBindingSource.EndEdit();
+            this.studentTableAdapter.Update(this.ackDataSet);
+            MessageBox.Show("Insert 성공");
+        }
+
+        private void Btn_Delete_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if(dataGridView1.Rows[i].Selected == true)
+                {
+                    dataGridView1.Rows.Remove(dataGridView1.Rows[i]);
+                }
+            }
+            this.Validate();
+            this.studentBindingSource.EndEdit();
+            this.studentTableAdapter.Update(this.ackDataSet);
+            MessageBox.Show("Delete 성공");
+        }
+    }
+}
